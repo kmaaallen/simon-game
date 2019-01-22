96a7755d3e1a1324297912271b1109ec440ec942
Kirsten Allen <kmaaallen@gmail.com>
2019-01-22 12:45:26 +0000

    power button functionality added to game logic file as tests passed

--- assets/js/gameLogic.js
+++ assets/js/gameLogic.js
@@ -2,4 +2,6 @@ jQuery(document).ready(function(){
     $.getScript('/assets/js/gameData.js');
 });
 
-
+document.getElementById('power').onclick = function(){
+gameData.powerStatus = !gameData.powerStatus;
+};
