854d2929bf2b6fd9c6a4ee2100cab8868265fcad
Kirsten Allen <kmaaallen@gmail.com>
2019-01-22 16:03:04 +0000

    added tested code for display into gameUI.js file

--- assets/js/gameUI.js
+++ assets/js/gameUI.js
@@ -1,3 +1,11 @@
 jQuery(document).ready(function(){
     $.getScript('/assets/js/gameLogic.js');
 });
+
+document.getElementById('power').addEventListener('click', function(){
+    if (gameData.powerStatus === true){
+        document.getElementById('display').innerHTML = "ready";
+    } else {
+        document.getElementById('display').innerHTML = '';
+    }
+})
