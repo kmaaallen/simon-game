deb0dfedbda677e897a7908c2dd5cfd33bb5673c
Kirsten Allen <kmaaallen@gmail.com>
2019-01-22 16:19:12 +0000

    tested code for displaying count on game start added to gameUI.js file

--- assets/js/gameUI.js
+++ assets/js/gameUI.js
@@ -6,6 +6,12 @@ document.getElementById('power').addEventListener('click', function(){
     if (gameData.powerStatus === true){
         document.getElementById('display').innerHTML = "ready";
     } else {
-        document.getElementById('display').innerHTML = '';
+         document.getElementById('display').innerHTML = '';
+    }
+});
+
+document.getElementById('start').addEventListener('click', function(){
+    if (gameData.powerStatus === true && gameData.startStatus === true){
+        document.getElementById('display').innerHTML = gameData.count;
     }
 })
