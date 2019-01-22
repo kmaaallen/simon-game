b8116fe35c38ac0bff2ede4e86a44ec7a12019ba
Kirsten Allen <kmaaallen@gmail.com>
2019-01-22 16:18:22 +0000

    specs added for display on starting game

--- documentation/automatedTesting/spec/uiSpec.js
+++ documentation/automatedTesting/spec/uiSpec.js
@@ -17,4 +17,15 @@ describe("game display test suite", function(){
             expect(document.getElementById('display').innerHTML).toBe('');
         });
     });
+    describe("displays count value once game has started", function(){
+        beforeEach(function(){
+            gameData.powerStatus = true;
+            gameData.startStatus = false;
+            gameData.count = 0;
+        });
+        it("should display count value of '1' when newGame is called (i.e it is the first round)", function(){
+            startClick();
+            expect(document.getElementById('display').innerHTML).toBe('1');
+        })
+    });
 });
