e7e325a2a9f1ca71068240224f97a4a637d0399f
Kirsten Allen <kmaaallen@gmail.com>
2019-01-23 10:40:13 +0000

    wrote and tested generateSequence function

--- documentation/automatedTesting/scripts/test.js
+++ documentation/automatedTesting/scripts/test.js
@@ -1,67 +1,71 @@
 // All script for testing is in same document here but separated in actual project.
 // Game Data Object ------------------------------------------------------------
 var gameData = {
-    powerStatus : false,
-    startStatus : false,
-    count:0
+    powerStatus: false,
+    startStatus: false,
+    count: 0,
+    gameSequence: []
 };
 
 // Game Logic ------------------------------------------------------------------
 //function pageReload(){
- //   location.reload();
+//   location.reload();
 //}
 
-function powerClick(){ // for testing purposes only
+function powerClick() { // for testing purposes only
     document.getElementById('power').click();
 }
 
-function startClick(){ // for testing purposes only
+function startClick() { // for testing purposes only
     document.getElementById('start').click();
 }
 
-document.getElementById('power').onclick = function(){
-gameData.powerStatus = !gameData.powerStatus;
-if (gameData.powerStatus === false){
-    gameData.startStatus = false;
-}
+document.getElementById('power').onclick = function() {
+    gameData.powerStatus = !gameData.powerStatus;
+    if (gameData.powerStatus === false) {
+        gameData.startStatus = false;
+    }
 };
 
-document.getElementById('start').onclick = function(){
-  gameData.startStatus = !gameData.startStatus;
-  if (gameData.startStatus === true){
-      newGame();
-  }
+document.getElementById('start').onclick = function() {
+    gameData.startStatus = !gameData.startStatus;
+    if (gameData.startStatus === true) {
+        newGame();
+    }
 };
 
-function newGame(){
-    gameData.count++;
+function newGame() {
     newRound();
 }
 
-function newRound(){
+function newRound() {
     generateSequence();
     displaySequence();
     playerInput();
 }
 
-function generateSequence(){}
+function generateSequence() {
+    gameData.gameSequence.push(Math.floor(Math.random() * 4 + 1));
+    gameData.count++;
+    displayCount();
+}
 
-function displaySequence(){}
+function displaySequence() {}
 
-function playerInput(){}
+function playerInput() {}
 
 
 // User Interface functions etc. -----------------------------------------------
-document.getElementById('power').addEventListener('click', function(){
-    if (gameData.powerStatus === true){
+document.getElementById('power').addEventListener('click', function() {
+    if (gameData.powerStatus === true) {
         document.getElementById('display').innerHTML = "ready";
-    } else {
-         document.getElementById('display').innerHTML = '';
+    }
+    else {
+        document.getElementById('display').innerHTML = '';
     }
 });
 
-document.getElementById('start').addEventListener('click', function(){
-    if (gameData.powerStatus === true && gameData.startStatus === true){
+function displayCount() {
         document.getElementById('display').innerHTML = gameData.count;
-    }
-})
+}
+
