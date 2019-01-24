65c1742ac5f11e416b9422bef33bd23f0a9a6e15
Kirsten Allen <kmaaallen@gmail.com>
2019-01-24 15:27:52 +0000

    specs added to test intervals between flashing lights when displaying sequence

--- documentation/automatedTesting/spec/uiSpec.js
+++ documentation/automatedTesting/spec/uiSpec.js
@@ -21,12 +21,12 @@ describe("game display test suite", function() {
         beforeEach(function() {
             gameData.gameSequence = [];
             gameData.count = 0;
-        })
+        });
         it("should display gameData.count when generateSequence function is called (as this is when count is incremented", function() {
             generateSequence();
             expect(document.getElementById('display').innerHTML).toBe('1');
-        })
-    })
+        });
+    });
 });
 
 describe("color functions test suite", function() {
@@ -81,3 +81,55 @@ describe("color functions test suite", function() {
         });
     });
 });
+
+describe("color flashing intervals test suite", function() {
+    beforeEach(function() {
+        jasmine.clock().install();
+        gameData.gameSequence = [1,2,3,4]
+        showSequence();
+    });
+    afterEach(function() {
+        jasmine.clock().uninstall();
+    });
+    describe("red color function should add and remove red-light class", function() {
+        it("should have class of red-light 1000ms after showSequence function called", function() {
+            jasmine.clock().tick(1000);
+            expect(document.getElementById('1').className).toBe('red-square red-light');
+        });
+        it("should remove class of red-light after 1500ms", function() {
+            jasmine.clock().tick(1500);
+            expect(document.getElementById('1').className).toBe('red-square');
+        });
+    });
+    describe("yellow color function should add yellow-light class", function() {
+        it("should have class of yellow-light 2000ms after showSequence function called", function() {
+            jasmine.clock().tick(2000);
+            expect(document.getElementById('2').className).toBe('yellow-square yellow-light');
+        });
+        it("should remove class of yellow-light after 2500ms", function() {
+            jasmine.clock().tick(2500);
+            expect(document.getElementById('2').className).toBe('yellow-square');
+        });
+    });
+    describe("green color function should add green-light class", function() {
+        it("should have class of green-light 3000ms after showSequence function called", function() {
+            jasmine.clock().tick(3000);
+            expect(document.getElementById('3').className).toBe('green-square green-light');
+        });
+        it("should remove class of green-light after 3500ms", function() {
+            jasmine.clock().tick(3500);
+            expect(document.getElementById('3').className).toBe('green-square');
+        });
+    });
+    describe("blue color function should add blue-light class", function() {
+        it("should have class of blue-light 4000ms after showSequence function called", function() {
+            jasmine.clock().tick(4000);
+            expect(document.getElementById('4').className).toBe('blue-square blue-light');
+        });
+        it("should remove class of blue-light after 4500ms", function() {
+            jasmine.clock().tick(4500);
+            expect(document.getElementById('4').className).toBe('blue-square');
+        });
+    });
+});
+
