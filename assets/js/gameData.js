4f17147e9bb548fe7c084422d7f9d23212ddcae3
Kirsten Allen <kmaaallen@gmail.com>
2019-01-29 17:25:45 +0000

    corrected links to different sounds

--- assets/js/gameData.js
+++ assets/js/gameData.js
@@ -13,6 +13,6 @@ var gameData = {
 };
 
 var redAudio = new Audio('/assets/sounds/simonSound1.mp3');
-var yellowAudio = new Audio('/assets/sounds/simonSound1.mp3');
-var greenAudio = new Audio('/assets/sounds/simonSound1.mp3');
-var blueAudio = new Audio('/assets/sounds/simonSound1.mp3');
+var yellowAudio = new Audio('/assets/sounds/simonSound2.mp3');
+var greenAudio = new Audio('/assets/sounds/simonSound3.mp3');
+var blueAudio = new Audio('/assets/sounds/simonSound4.mp3');
