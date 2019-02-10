b2ae290ccbf3c69a5fd8493527d30409725207e8
Kirsten Allen <kmaaallen@gmail.com>
2019-02-10 17:54:31 +0000

    moved segment object into gameUI

--- assets/js/gameData.js
+++ assets/js/gameData.js
@@ -9,13 +9,6 @@ var gameData = {
     strictStatus: false,
 };
 
-var segment = {
-    red: document.getElementById('1'),
-    yellow: document.getElementById('2'),
-    green: document.getElementById('3'),
-    blue: document.getElementById('4')
-}
-
 var redAudio = new Audio('assets/sounds/simonSound1.mp3');
 var yellowAudio = new Audio('assets/sounds/simonSound2.mp3');
 var greenAudio = new Audio('assets/sounds/simonSound3.mp3');
