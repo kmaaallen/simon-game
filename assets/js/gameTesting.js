025e181d36b5b60e600311a9ba962713ad8fb45c
Kirsten Allen <kmaaallen@gmail.com>
2019-03-18 20:35:10 +0000

    added gameTesting js file which contains functions needed only for testing not for game play

---
+++ assets/js/gameTesting.js
@@ -0,0 +1,36 @@
+//Functions for testing purposes only
+function powerClick() { // for testing purposes only
+    document.getElementById('power-toggle').click();
+}
+
+function startClick() { // for testing purposes only
+    document.getElementById('start').click();
+}
+
+function strictClick() { // for testing purposes only
+    document.getElementById('strict-toggle').click();
+}
+
+function redClick() { // for testing purposes only
+    document.getElementById('1').click();
+}
+
+function yellowClick() { // for testing purposes only
+    document.getElementById('2').click();
+}
+
+function greenClick() { // for testing purposes only
+    document.getElementById('3').click();
+}
+
+function blueClick() { // for testing purposes only
+    document.getElementById('4').click();
+}
+
+function newGame() {
+    gameData.count = 0;
+    gameData.gameSequence = [];
+    gameData.playerSequence = [];
+    setTimeout(newRound, 11); // set timeout only for testing purposes to check game variables reset
+}
+// ----------------------------------------------------
