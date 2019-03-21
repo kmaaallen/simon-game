6b9d18b38f07985d20e0c8bbce1fdfd7cd85f47d
Kirsten Allen <kmaaallen@gmail.com>
2019-03-21 21:08:03 +0000

    added closing brackets for test suite

--- documentation/automatedTesting/spec/uiSpec.js
+++ documentation/automatedTesting/spec/uiSpec.js
@@ -94,90 +94,90 @@ describe("color functions test suite", function() {
 });
 
 describe("player input visual/audio test suite", function() {
-            beforeEach(function() {
-                playerInput();
-            });
-            describe("player should see colour flash corresponding to their choice made by clicking on square", function() {
-                it("should call colour function with (redAudio, 1, 'red-light') when red square is clicked", function() {
-                    spyOn(window, 'colour');
-                    redClick();
-                    expect(window.colour).toHaveBeenCalledWith(redAudio, 1, 'red-light');
-                });
-                it("should call colour function with (yellowAudio, 2, 'yellow-light') when yellow square is clicked", function() {
-                    spyOn(window, 'colour');
-                    yellowClick();
-                    expect(window.colour).toHaveBeenCalledWith(yellowAudio, 2, 'yellow-light');
-                });
-                it("should call colour function with (greenAudio, 3, 'green-light') when green square is clicked", function() {
-                    spyOn(window, 'colour');
-                    greenClick();
-                    expect(window.colour).toHaveBeenCalledWith(greenAudio, 3, 'green-light');
-                });
-                it("should call colour function with (blueAudio, 4, 'blue-light') when blue square is clicked", function() {
-                    spyOn(window, 'colour');
-                    blueClick();
-                    expect(window.colour).toHaveBeenCalledWith(blueAudio, 4, 'blue-light');
-                });
-            });
+    beforeEach(function() {
+        playerInput();
+    });
+    describe("player should see colour flash corresponding to their choice made by clicking on square", function() {
+        it("should call colour function with (redAudio, 1, 'red-light') when red square is clicked", function() {
+            spyOn(window, 'colour');
+            redClick();
+            expect(window.colour).toHaveBeenCalledWith(redAudio, 1, 'red-light');
+        });
+        it("should call colour function with (yellowAudio, 2, 'yellow-light') when yellow square is clicked", function() {
+            spyOn(window, 'colour');
+            yellowClick();
+            expect(window.colour).toHaveBeenCalledWith(yellowAudio, 2, 'yellow-light');
+        });
+        it("should call colour function with (greenAudio, 3, 'green-light') when green square is clicked", function() {
+            spyOn(window, 'colour');
+            greenClick();
+            expect(window.colour).toHaveBeenCalledWith(greenAudio, 3, 'green-light');
+        });
+        it("should call colour function with (blueAudio, 4, 'blue-light') when blue square is clicked", function() {
+            spyOn(window, 'colour');
+            blueClick();
+            expect(window.colour).toHaveBeenCalledWith(blueAudio, 4, 'blue-light');
+        });
+    });
+});
 
+describe("player messages when sequence checked test suite", function() {
+    describe("player should get win message when game won", function() {
+        it("should display a 'Win!' message when game is won", function() {
+            gameData.gameSequence = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+            gameData.playerSequence = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+            spyOn(window, 'displayMessage');
+            checkSequence();
+            expect(window.displayMessage).toHaveBeenCalledWith(display.win);
+        });
+        it("should display a 'Try again!' message when playerSequence is incorrect and strict mode off", function() {
+            gameData.gameSequence = [1, 2, 3, 4];
+            gameData.playerSequence = [1, 2, 4, 3];
+            gameData.strictStatus = false;
+            spyOn(window, 'displayMessage');
+            checkSequence();
+            expect(window.displayMessage).toHaveBeenCalledWith(display.tryAgain);
+        });
+    });
+});
 
-            describe("player messages when sequence checked test suite", function() {
-                describe("player should get win message when game won", function() {
-                    it("should display a 'Win!' message when game is won", function() {
-                        gameData.gameSequence = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
-                        gameData.playerSequence = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
-                        spyOn(window, 'displayMessage');
-                        checkSequence();
-                        expect(window.displayMessage).toHaveBeenCalledWith(display.win);
-                    });
-                    it("should display a 'Try again!' message when playerSequence is incorrect and strict mode off", function() {
-                        gameData.gameSequence = [1, 2, 3, 4];
-                        gameData.playerSequence = [1, 2, 4, 3];
-                        gameData.strictStatus = false;
-                        spyOn(window, 'displayMessage');
-                        checkSequence();
-                        expect(window.displayMessage).toHaveBeenCalledWith(display.tryAgain);
-                    });
-                });
-            });
+describe("strictmode fail display", function() {
+    it("should display start again message when player wrong in strict mode", function() {
+        displayMessage(display.startAgain);
+        expect(document.getElementById('display').innerHTML).toBe('Start Again!');
+    });
+});
 
-            describe("strictmode fail display", function() {
-                it("should display start again message when player wrong in strict mode", function() {
-                    displayMessage(display.startAgain);
-                    expect(document.getElementById('display').innerHTML).toBe('Start Again!');
-                });
-            });
+describe("strictmode button display test suite", function() {
+    describe("strict button appearance depending on mode", function() {
+        it("should display background of green and toggle on right when strict mode is on", function() {
+            gameData.strictStatus = false;
+            strictClick();
+            expect(document.getElementById('strict-btn').className).toBe('btn-after');
+            expect(document.getElementById('strict-toggle').className).toBe('positionAfter');
+        });
+        it("should display background of red and toggle on left when strict mode is off", function() {
+            gameData.strictStatus = true;
+            strictClick();
+            expect(document.getElementById('strict-btn').className).toBe('btn-before');
+            expect(document.getElementById('strict-toggle').className).toBe('positionBefore');
+        });
+    });
+});
 
-            describe("strictmode button display test suite", function() {
-                describe("strict button appearance depending on mode", function() {
-                    it("should display background of green and toggle on right when strict mode is on", function() {
-                        gameData.strictStatus = false;
-                        strictClick();
-                        expect(document.getElementById('strict-btn').className).toBe('btn-after');
-                        expect(document.getElementById('strict-toggle').className).toBe('positionAfter');
-                    });
-                    it("should display background of red and toggle on left when strict mode is off", function() {
-                        gameData.strictStatus = true;
-                        strictClick();
-                        expect(document.getElementById('strict-btn').className).toBe('btn-before');
-                        expect(document.getElementById('strict-toggle').className).toBe('positionBefore');
-                    });
-                });
-            });
-
-            describe("power button display test suite", function() {
-                describe("power button appearance depending on status", function() {
-                    it("should display background of green and toggle on right when power is on", function() {
-                        gameData.powerStatus = false;
-                        powerClick();
-                        expect(document.getElementById('power-btn').className).toBe('btn-after');
-                        expect(document.getElementById('power-toggle').className).toBe('positionAfter');
-                    });
-                    it("should display background of red and toggle on left when power is off", function() {
-                        gameData.powerStatus = true;
-                        powerClick();
-                        expect(document.getElementById('power-btn').className).toBe('btn-before');
-                        expect(document.getElementById('power-toggle').className).toBe('positionBefore');
-                    });
-                });
-            });
+describe("power button display test suite", function() {
+    describe("power button appearance depending on status", function() {
+        it("should display background of green and toggle on right when power is on", function() {
+            gameData.powerStatus = false;
+            powerClick();
+            expect(document.getElementById('power-btn').className).toBe('btn-after');
+            expect(document.getElementById('power-toggle').className).toBe('positionAfter');
+        });
+        it("should display background of red and toggle on left when power is off", function() {
+            gameData.powerStatus = true;
+            powerClick();
+            expect(document.getElementById('power-btn').className).toBe('btn-before');
+            expect(document.getElementById('power-toggle').className).toBe('positionBefore');
+        });
+    });
+});
