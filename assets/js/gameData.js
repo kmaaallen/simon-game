6a7338e1823f55df1fb30c8b2df8ad02e3c504b5
Kirsten Allen <kmaaallen@gmail.com>
2019-03-24 09:27:54 +0000

    corrected links to simon sounds

--- assets/js/gameData.js
+++ assets/js/gameData.js
@@ -10,7 +10,7 @@ var gameData = {
 };
 
 var redAudio = new Audio('https://simon-game-3-kmaaallen.c9users.io/assets/sounds/simonSound1.mp3');
-var yellowAudio = new Audio('https://simon-game-3-kmaaallen.c9users.io/assets/sounds/simonSound1.mp3');
-var greenAudio = new Audio('https://simon-game-3-kmaaallen.c9users.io/assets/sounds/simonSound1.mp3');
-var blueAudio = new Audio('https://simon-game-3-kmaaallen.c9users.io/assets/sounds/simonSound1.mp3');
+var yellowAudio = new Audio('https://simon-game-3-kmaaallen.c9users.io/assets/sounds/simonSound2.mp3');
+var greenAudio = new Audio('https://simon-game-3-kmaaallen.c9users.io/assets/sounds/simonSound3.mp3');
+var blueAudio = new Audio('https://simon-game-3-kmaaallen.c9users.io/assets/sounds/simonSound4.mp3');
 
